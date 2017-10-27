1af7b186d38cb427228131c7a67607a4a969b693
Philip Jägenstedt <philip@foolip.org>
2017-10-27 16:05:11 +0200

    Use branches option in .travis.yml instead of env var checks

--- run-tests.sh
+++ run-tests.sh
@@ -3,13 +3,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# do nothing on Travis except for the master branch
-TRAVIS=${TRAVIS:-false}
-if [[ "$TRAVIS" == "true" && "$TRAVIS_BRANCH" != "master" ]]; then
-    echo "Skipping tests for branch $TRAVIS_BRANCH (not master)"
-    exit 0
-fi
-
 cd tests
 npm install node-fetch
 
