ad17fd1b59e626cc019672b3d2c5fba661825dbd
Philip Jägenstedt <philip@foolip.org>
2017-11-06 21:13:38 -0800

    Cache fonts and images for 7 days
    Fixes https://github.com/whatwg/misc-server/issues/10.

---
+++ test/caching.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const assert = require('assert');
+const fetch = require('node-fetch');
+
+const CACHE_TESTS = [
+  'https://html.spec.whatwg.org/fonts/Essays1743.ttf',
+  'https://html.spec.whatwg.org/images/abstract.jpeg',
+  'https://html.spec.whatwg.org/images/wolf.jpg',
+  'https://images.whatwg.org/abstract.png',
+  'https://images.whatwg.org/content-venn.svg',
+  'https://images.whatwg.org/logo',
+  'https://images.whatwg.org/robots.jpeg',
+  'https://resources.whatwg.org/browser-logos/bb.jpg',
+  'https://resources.whatwg.org/browser-logos/edge.svg',
+  'https://resources.whatwg.org/fonts/SourceSansPro-Regular.woff',
+  'https://resources.whatwg.org/fonts/SourceSansPro-Regular.woff2',
+  'https://resources.whatwg.org/logo.png',
+];
+
+const NO_CACHE_TESTS = [
+  'https://html.spec.whatwg.org/multipage/',
+  'https://images.whatwg.org/README.txt',
+  'https://resources.whatwg.org/standard.css',
+];
+
+function test(url, expected) {
+  specify(url, async function() {
+    // redirecting is a failure since we might then test the wrong server
+    const response = await fetch(url, { redirect: 'manual' });
+    assert.strictEqual(response.status, 200);
+    assert.strictEqual(response.headers.get('cache-control'), expected);
+  });
+}
+
+describe('caching', function() {
+  describe('URLs with cache-control header', function() {
+    for (const url of CACHE_TESTS) {
+      let expected = 'max-age=604800'; // 7 days
+      // TODO: remove this hack when HTML has new hosting
+      if (url.startsWith('https://html.spec.whatwg.org/')) {
+        expected = 'public';
+      }
+      test(url, expected);
+    }
+  });
+  describe('URLs without cache-control', function() {
+    for (const url of NO_CACHE_TESTS) {
+      test(url, null);
+    }
+  });
+});
