117f81b10112689743daae08d91ac69b2c937bbe
Philip Jägenstedt <philip@foolip.org>
2017-11-06 21:16:17 -0800

    Add tests to notice if the wiki is broken

---
+++ test/wiki.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const assert = require('assert');
+const fetch = require('node-fetch');
+
+describe('wiki', function() {
+  specify('main page', async function() {
+    const response = await fetch('https://wiki.whatwg.org/wiki/Main_Page');
+    assert.strictEqual(response.status, 200);
+    const text = await response.text();
+    // the main page should link to the FAQ
+    assert(text.includes('https://whatwg.org/faq'));
+    assert(!text.includes('This site is experiencing technical difficulties'));
+  });
+
+  specify('history', async function() {
+    const response = await fetch('https://wiki.whatwg.org/index.php?title=IRC&action=history');
+    assert.strictEqual(response.status, 200);
+    const text = await response.text();
+    // history pages should have a certain title and buttons
+    assert(text.includes('Revision history of "IRC"'));
+    assert(text.includes('value="Compare selected revisions"'));
+    assert(!text.includes('This site is experiencing technical difficulties'));
+  });
+});
