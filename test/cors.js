8577fb1e03053b43b5375c6e07c3f76ad6c6f2d4
Philip Jägenstedt <philip@foolip.org>
2018-01-07 22:16:48 +0100

    Add CORS headers for all static sites (marquee)
    Fixes https://github.com/whatwg/misc-server/issues/68.

---
+++ test/cors.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const assert = require('assert');
+const fetch = require('node-fetch');
+
+const CORS_TESTS = [
+  // not an exhaustive list of domains, just enough to catch accidental removal
+  'https://dom.spec.whatwg.org/',
+  'https://html.spec.whatwg.org/',
+  'https://resources.whatwg.org/',
+  'https://whatwg.org/',
+];
+
+const NO_CORS_TESTS = [
+  // domains for which it's probably a mistake to have CORS headers
+  'https://blog.whatwg.org/',
+  //'https://lists.whatwg.org/',
+  'https://participate.whatwg.org/',
+  'https://wiki.whatwg.org/',
+];
+
+function test(url, expected) {
+  specify(url, async function() {
+    // redirecting is a failure since we might then test the wrong server
+    const response = await fetch(url, { redirect: 'manual' });
+    assert.strictEqual(response.status, 200);
+    assert.strictEqual(response.headers.get('access-control-allow-origin'), expected);
+  });
+}
+
+describe('access-control-allow-origin header', function() {
+  describe('URLs with header', function() {
+    for (const url of CORS_TESTS) {
+      test(url, '*');
+    }
+  });
+  describe('URLs without header', function() {
+    for (const url of NO_CORS_TESTS) {
+      test(url, null);
+    }
+  });
+});
