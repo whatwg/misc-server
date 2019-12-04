9a37cc06612a1c04ab7042b914530f2f8ea0c8f3
Philip Jägenstedt <philip@foolip.org>
2019-12-04 13:40:28 +0100

    Stop sending x-xss-protection
    Fixes https://github.com/whatwg/misc-server/issues/114.

--- test/x-headers.js
+++ test/x-headers.js
@@ -5,26 +5,27 @@ const fetch = require('node-fetch');
 
 const TEST_DATA = [
   // not an exhaustive list, just enough to catch accidental removal
-  ['whatwg.org', 'nosniff', null, '1; mode=block'],
-  ['blog.whatwg.org', 'nosniff', 'sameorigin', '1; mode=block'],
-  ['dom.spec.whatwg.org', 'nosniff', null, '1; mode=block'],
-  ['participate.whatwg.org', 'nosniff', 'deny', '1; mode=block'],
-  ['resources.whatwg.org', 'nosniff', null, '1; mode=block'],
-  ['spec.whatwg.org', 'nosniff', null, '1; mode=block'],
+  ['whatwg.org', 'nosniff', null],
+  ['blog.whatwg.org', 'nosniff', 'sameorigin'],
+  ['dom.spec.whatwg.org', 'nosniff', null],
+  ['participate.whatwg.org', 'nosniff', 'deny'],
+  ['resources.whatwg.org', 'nosniff', null],
+  ['spec.whatwg.org', 'nosniff', null],
   // FIXME: don't send double x-content-type-options headers
   // https://github.com/whatwg/misc-server/issues/115
-  ['wiki.whatwg.org', 'nosniff, nosniff', 'sameorigin', '1; mode=block'],
+  ['wiki.whatwg.org', 'nosniff, nosniff', 'sameorigin'],
 ];
 
 describe('x-* headers', function() {
-  for (const [hostname, cto, xfo, xss] of TEST_DATA) {
+  for (const [hostname, cto, xfo] of TEST_DATA) {
     specify(hostname, async function() {
       // redirecting is a failure since we might then test the wrong server
       const response = await fetch(`https://${hostname}/`, { redirect: 'manual' });
       assert.strictEqual(response.status, 200);
       assert.strictEqual(response.headers.get('x-content-type-options'), cto);
       assert.strictEqual(response.headers.get('x-frame-options'), xfo);
-      assert.strictEqual(response.headers.get('x-xss-protection'), xss);
+      // Expect no x-xss-protection header
+      assert.strictEqual(response.headers.get('x-xss-protection'), null);
     });
   }
 });
