68071f373614e91fa267cddbd3583b0819fb0894
Philip Jägenstedt <philip@foolip.org>
2019-12-05 11:35:59 +0100

    Update build.js

--- test/build.js
+++ test/build.js
@@ -3,7 +3,7 @@
 const assert = require('assert');
 const fetch = require('node-fetch');
 
-describe.only('build', function() {
+describe('build', function() {
   specify('/ping', async function() {
     const response = await fetch('https://build.whatwg.org/ping');
     assert.strictEqual(response.status, 200);
